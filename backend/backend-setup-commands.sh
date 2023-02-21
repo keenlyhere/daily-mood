0f6afb59f66353e408dfd5e367143fd057b7a843
Keenly <keenlyhere@gmail.com>
2023-02-21 08:21:33 -0800

    Initialize Express and Sequelize with CSRF protection

---
+++ backend/backend-setup-commands.sh
@@ -0,0 +1,2 @@
+npm install cookie-parser cors csurf dotenv express express-async-errors helmet jsonwebtoken morgan per-env sequelize@6 sequelize-cli@6 pg
+npm install -D sqlite3 dotenv-cli nodemon
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,10 +5,10 @@ const apiRouter = require('./api');
 router.use('/api', apiRouter);
 
 // for testing purposes
-// router.get('/hello/world', function(req, res) {
-//   res.cookie('XSRF-TOKEN', req.csrfToken());
-//   res.send('Hello World!');
-// });
+router.get('/hello/world', function(req, res) {
+  res.cookie('XSRF-TOKEN', req.csrfToken());
+  res.send('Hello World!');
+});
 
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
