5b1889ec1473a3fae5899d110aea331ac6ee830f
Keenly <keenlyhere@gmail.com>
2023-02-21 11:16:03 -0800

    Add User input validation on user login backend endpoint

--- backend/backend-setup-commands.sh
+++ backend/backend-setup-commands.sh
@@ -1,4 +1,4 @@
-npm install cookie-parser cors csurf dotenv express express-async-errors helmet jsonwebtoken morgan per-env sequelize@6 sequelize-cli@6 pg bcryptjs
+npm install cookie-parser cors csurf dotenv express express-async-errors helmet jsonwebtoken morgan per-env sequelize@6 sequelize-cli@6 pg bcryptjs express-validator
 npm install -D sqlite3 dotenv-cli nodemon
 
 
