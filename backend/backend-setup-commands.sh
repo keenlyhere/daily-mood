b33fc2e6672a37d9366d9e65f2f14094b1f5dd52
Keenly <keenlyhere@gmail.com>
2023-02-21 15:30:43 -0800

    Potential render fix

--- backend/backend-setup-commands.sh
+++ backend/backend-setup-commands.sh
@@ -1,12 +1,2 @@
 npm install cookie-parser cors csurf dotenv express express-async-errors helmet jsonwebtoken morgan per-env sequelize@6 sequelize-cli@6 pg bcryptjs express-validator
 npm install -D sqlite3 dotenv-cli nodemon
-
-
-npx sequelize model:generate --name User --attributes email:string,firstName:string,lastName:string,hashedPassword:string,birthday:dateonly,displayPic:string,theme:string,moolah:integer,activePet:integer,activeBg:integer
-
-npx sequelize model:generate --name Pet --attributes name:string,flavor:string,health:integer,friendliness:integer,petImageUrl:string,userItemId:integer
-
-
-npx sequelize model:generate --name Background --attributes bgName:string,bgImageUrl:string,userItemId:integer
-
-npx sequelize seed:generate --name add-pets
