ecf8070cdc5c520dc985adde549752615a8db6d2
Keenly <keenlyhere@gmail.com>
2023-02-21 12:22:31 -0800

    Setup custom csrfFetch

---
+++ dp-frontend/src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import App from './App';
+import configureStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
+import './index.css';
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== "production") {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+}
+
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
