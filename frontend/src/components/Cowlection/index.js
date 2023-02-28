761a28740a4169afe597f1a5c86acdf6664eec51
Keenly <keenlyhere@gmail.com>
2023-02-28 10:53:45 -0800

    Created tabs for cows and backgroundds in Cowlection page

--- frontend/src/components/Cowlection/index.js
+++ frontend/src/components/Cowlection/index.js
@@ -8,6 +8,7 @@ import "./Cowlection.css";
 export default function Cowlection({ user }) {
     const dispatch = useDispatch();
     const [ isLoaded, setIsLoaded ] = useState(false);
+    const [ page, setPage ] = useState("cows");
     const activePet = useSelector(state => state.items.activePet);
     const userPets = useSelector(state => state.items.pets);
     // console.log("userPets ===>", userPets)
@@ -19,6 +20,7 @@ export default function Cowlection({ user }) {
     }, [dispatch])
 
     if (isLoaded) {
+
 
         Object.values(userPets).forEach(pet => userFlavors.push(pet.flavor));
         console.log("userFlavors", userFlavors);
@@ -42,40 +44,82 @@ export default function Cowlection({ user }) {
 
         return (
             <div className="Cowlection-container">
-                <h1>Your udderly adorable herd</h1>
-                <div className="Cowlection-cows">
-                        { cows.userCowImages.map((pet, idx) => (
-                            <div key={idx} className="Pet-gachapon-card">
-                                <div className="Pet-gachapon-card-image">
-                                    <img src={pet} className={`${userFlavors.includes(cows.userCowFlavors[idx]) ? "" : "notOwned"}`} alt="Gachapon pet prize" />
-                                </div>
-                                <div className="Pet-gachapon-description">
-                                    <h2 className="Pet-gachapon-flavor">
-                                        {cows.userCowFlavors[idx]}
-                                    </h2>
+                <div className="Cowlection-tabs">
+                    <input
+                        type="radio"
+                        value="cows"
+                        id="cows"
+                        name="tabs"
+                        checked={page === "cows"}
+                        onChange={() => setPage("cows")}
+                    />
+                    <label
+                        htmlFor="cows"
+                        className="Cowlection-tab clickable"
+                    >
+                        Cows
+                    </label>
+                    <input
+                        type="radio"
+                        value="backgrounds"
+                        id="backgrounds"
+                        name="tabs"
+                        checked={page === "backgrounds"}
+                        onChange={() => setPage("backgrounds")}
+                    />
+                    <label
+                        htmlFor="backgrounds"
+                        className="Cowlection-tab clickable"
+                    >
+                        Backgrounds
+                    </label>
+                    <span class="slider"></span>
+                </div>
+                { page === "cows" && (
+                    <>
+                        <h1>Your udderly adorable herd</h1>
+                        <div className="Cowlection-cows">
+                            { cows.userCowImages.map((pet, idx) => (
+                                <div key={idx} className="Pet-gachapon-card">
+                                    <div className="Pet-gachapon-card-image">
+                                        <img src={pet} className={`${userFlavors.includes(cows.userCowFlavors[idx]) ? "" : "notOwned"}`} alt="Gachapon pet prize" />
+                                    </div>
+                                    <div className="Pet-gachapon-description">
+                                        <h2 className="Pet-gachapon-flavor">
+                                            {cows.userCowFlavors[idx]}
+                                        </h2>
 
-                                    {
-                                        activePet.flavor === cows.userCowFlavors[idx] ? (
-                                            <button
-                                                className="Pet-gachapon-play disabled"
-                                                disabled={true}
-                                            >
-                                                Active
-                                            </button>
-                                        ) : (
-                                            <button
-                                                className="Pet-gachapon-play"
-                                                onClick={() => changeActivePet(cows.userCowFlavors[idx])}
-                                            >
-                                                Set active
-                                            </button>
-                                        )
-                                    }
+                                        {
+                                            activePet.flavor === cows.userCowFlavors[idx] ? (
+                                                <button
+                                                    className="Pet-gachapon-play disabled"
+                                                    disabled={true}
+                                                >
+                                                    Active
+                                                </button>
+                                            ) : (
+                                                <button
+                                                    className="Pet-gachapon-play"
+                                                    onClick={() => changeActivePet(cows.userCowFlavors[idx])}
+                                                >
+                                                    Set active
+                                                </button>
+                                            )
+                                        }
 
+                                    </div>
                                 </div>
-                            </div>
-                        ))}
-                    </div>
+                            ))}
+                        </div>
+                    </>
+                )}
+                {
+                    page === "backgrounds" && (
+                        <>
+                            <h1>Your backgrounds here</h1>
+                        </>
+                    )
+                }
             </div>
         )
     } else {
