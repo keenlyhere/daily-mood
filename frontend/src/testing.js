fcf4be5be1e36408b2d0561f65bf6c92d3423e38
Keenly <keenlyhere@gmail.com>
2023-05-15 14:16:43 -0700

    Fixed deployment errors

--- frontend/src/testing.js
+++ frontend/src/testing.js
@@ -1,42 +1,42 @@
-import { useState } from "react";
-import DayPicker from "./components/SignupFormModal/DatePicker/DayPicker";
-import MonthPicker from "./components/SignupFormModal/DatePicker/MonthPicker";
-import YearPicker from "./components/SignupFormModal/DatePicker/YearPicker";
-import { monthToNum } from "./utils/dateFormating";
+// import { useState } from "react";
+// import DayPicker from "./components/SignupFormModal/DatePicker/DayPicker";
+// import MonthPicker from "./components/SignupFormModal/DatePicker/MonthPicker";
+// import YearPicker from "./components/SignupFormModal/DatePicker/YearPicker";
+// import { monthToNum } from "./utils/dateFormating";
 
-export default function Testing() {
-    // const [ chosenYear, setChosenYear ] = useState(null);
-    // const [ chosenMonth, setChosenMonth ] = useState(null);
-    // const [ chosenDay, setChosenDay ] = useState(null);
-    const [year, setYear] = useState("");
-    const [month, setMonth] = useState("");
-    const [day, setDay] = useState("");
+// export default function Testing() {
+//     // const [ chosenYear, setChosenYear ] = useState(null);
+//     // const [ chosenMonth, setChosenMonth ] = useState(null);
+//     // const [ chosenDay, setChosenDay ] = useState(null);
+//     const [year, setYear] = useState("");
+//     const [month, setMonth] = useState("");
+//     const [day, setDay] = useState("");
 
-    const handleYearChange = (selectedYear) => {
-        setYear(selectedYear);
-        // setChosenYear(selectedYear);
-    };
+//     const handleYearChange = (selectedYear) => {
+//         setYear(selectedYear);
+//         // setChosenYear(selectedYear);
+//     };
 
-    const handleMonthChange = (selectedMonth) => {
-        setMonth(selectedMonth);
-        // setChosenMonth(selectedMonth);
-    };
+//     const handleMonthChange = (selectedMonth) => {
+//         setMonth(selectedMonth);
+//         // setChosenMonth(selectedMonth);
+//     };
 
-    const handleDayChange = (selectedDay) => {
-        setDay(selectedDay);
-        // setChosenDay(selectedDay);
-    };
+//     const handleDayChange = (selectedDay) => {
+//         setDay(selectedDay);
+//         // setChosenDay(selectedDay);
+//     };
 
-    "selectedYear ==>", year;
-    // console.log("selectedMonth ==>", month);
-    // console.log("selectedDay ==>", day);
-    // console.log("FULL DAY ==>", new Date(year, monthToNum[month] - 1, day));
+//     "selectedYear ==>", year;
+//     // console.log("selectedMonth ==>", month);
+//     // console.log("selectedDay ==>", day);
+//     // console.log("FULL DAY ==>", new Date(year, monthToNum[month] - 1, day));
 
-    return (
-        <div className="Testing-container">
-            <YearPicker chosenYear={year} onChange={handleYearChange} />
-            <MonthPicker chosenMonth={month} chosenYear={year} onChange={handleMonthChange} />
-            <DayPicker chosenDay={day} chosenYear={year} chosenMonth={month} onChange={handleDayChange} />
-        </div>
-    );
-}
+//     return (
+//         <div className="Testing-container">
+//             <YearPicker chosenYear={year} onChange={handleYearChange} />
+//             <MonthPicker chosenMonth={month} chosenYear={year} onChange={handleMonthChange} />
+//             <DayPicker chosenDay={day} chosenYear={year} chosenMonth={month} onChange={handleDayChange} />
+//         </div>
+//     );
+// }
